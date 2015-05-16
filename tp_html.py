9938099a9ba3def83373e7eff517f15e5bd34255
Cédric Donner <cedonner@gmail.com>
2015-05-16 11:59:10 +0000

    ajout fichier template pour tp_html.py (à compléter)

---
+++ tp_html.py
@@ -0,0 +1,27 @@
+from csudoci.html.parser import html_to_tree
+
+def get_elements(tree, selectors):
+    '''
+    
+    Filtre l'arbre et ne renvoie que les éléments dont la balise figure dans la liste
+    ``selectors``. Ce sont des sous-arbres complets qui sont renvoyés puisque l'éléments
+    racine fait référence à tout le sous-arbre.
+    
+    '''
+    
+    # compléter la fonction ici
+    pass
+    
+def table_matieres(root, level=2):
+    # à compléter ...
+    pass
+    
+    
+def remove_links(root):
+    # à compléter ...
+    pass
+    
+    
+
+if __name__ == '__main__':
+    pass
