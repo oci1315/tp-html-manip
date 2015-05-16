19877dc7d7970b02adfe20263b05acf32fda39a2
Cédric Donner <cedonner@gmail.com>
2015-05-16 11:55:45 +0000

    ajout du corrigé

---
+++ tp_html.py
@@ -0,0 +1,57 @@
+from csudoci.html.parser import HTMLTreeParser, html_to_tree
+from csudoci.html.htmltree import Div, Span, T, E
+
+def get_elements(tree, selectors):
+    '''
+    
+    Filtre l'arbre et ne renvoie que les éléments dont la balise figure dans la liste
+    ``selectors``. Ce sont des sous-arbres complets qui sont renvoyés puisque l'éléments
+    racine fait référence à tout le sous-arbre.
+    
+    '''
+    
+    elements = []
+    root = tree
+    
+    if root.tag in selectors:
+        elements.append(tree)
+    else:
+        for c in tree.children:
+            elements += get_elements(c, selectors)
+
+    return elements
+    
+    
+def table_matieres(root, level=2):
+    hlevels = ['h' + str(lev+1) for lev in range(level)]
+    headers = get_elements(root, hlevels)
+    return headers
+    
+    
+def remove_links(root):
+    
+    for child in root.children:
+        remove_links(child)
+        
+    if isinstance(root, T):
+        return root
+        
+    if root.tag == 'a':
+        if root.parent:
+            # aller dans le parent et remplacer l'élément courant par ses enfants
+            siblings = root.parent.children
+            index = siblings.index(root)
+            siblings[index:index+1] = root.children
+            root.parent.children = siblings
+            root.parent = None
+            
+        else:
+            span = Span()
+            span > root.children
+            return span
+        
+    return root
+    
+
+if __name__ == '__main__':
+    pass
